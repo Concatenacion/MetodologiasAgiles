a044718bd82a06ad6bd74fbad35ffef5918b43da
Concatenacion <pedroforton@gmail.com>
2017-04-07 10:18:58 +0200

    Actualizacion de la 2 version

--- app/src/main/java/com/mayc/unizar/app/adapters/DbAdapter.java
+++ app/src/main/java/com/mayc/unizar/app/adapters/DbAdapter.java
@@ -1,4 +1,4 @@
-package zgz.nasa.spaceapps.awesomoon.CustomAdapter;
+package com.mayc.unizar.app.adapters;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -8,18 +8,17 @@ import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import com.mayc.unizar.app.SQLiteRelacional;
 
-import zgz.nasa.spaceapps.awesomoon.SQLiteRelacional;
 
 
-/**
- * Created by Guillermo on 9/03/16.
- */
 public class DbAdapter extends SQLiteRelacional {
+
     private final Context mCtx;
     private DatabaseHelper mDbHelper;
     private static SQLiteDatabase mDb;
-    private static final String TAG =" DBADAPTER";
+    private static final String TAG = " DBADAPTER";
+
     /**
      * Constructor - takes the context to allow the database to be
      * opened/created
@@ -45,25 +44,13 @@ public class DbAdapter extends SQLiteRelacional {
         return this;
     }
 
-    public boolean isOpen(){
-        return  mDb.isOpen();
+    public boolean isOpen() {
+        return mDb.isOpen();
     }
 
     public void close() {
         mDbHelper.close();
     }
-
-   /*public boolean updateFavorite(String path, boolean isFavorite) {
-        ContentValues args = new ContentValues();
-        String value;
-        if(isFavorite){
-            value="1";
-        }else{
-            value="0";
-        }
-        args.put(KEY_FAVORITO, value);
-        return mDb.update(DATABASE_TABLE_CANCION, args, KEY_RUTA + " =?", new String[]{path}) > 0;
-    }*/
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -74,380 +61,67 @@ public class DbAdapter extends SQLiteRelacional {
         @Override
         public void onCreate(SQLiteDatabase db) {
 
-            db.execSQL(DATABASE_CREATE_INFORMACION);
-            db.execSQL(DATABASE_CREATE_MULTIMEDIA);
-            db.execSQL(DATABASE_CREATE_PREGUNTA);
-            db.execSQL(DATABASE_CREATE_PARTIDA);
-            db.execSQL(DATABASE_CREATE_CONTIENE);
-            db.execSQL(DATABASE_CREATE_MUESTRA);
+            db.execSQL(DATABASE_CREATE_HISTORIAS);
+            db.execSQL(DATABASE_CREATE_TARJETAS);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             Log.w("DbAdapter", "Upgrading database from version " + oldVersion + " to "
                     + newVersion + ", which will destroy all old data");
-            db.execSQL("DROP TABLE IF EXISTS notes");
+            db.execSQL("DROP TABLE IF EXISTS DATABASE_TABLE_HISTORIAS");
+            db.execSQL("DROP TABLE IF EXISTS DATABASE_CREATE_TARJETAS");
             onCreate(db);
         }
     }
-    public long insertInformacion(int id, String titulo, String cuerpo, String uriinfo){
-            ContentValues initialValues = new ContentValues();
-            initialValues.put(KEY_IDINFO,id);
-            initialValues.put(Titulo, titulo);
-            initialValues.put(INFOURI,uriinfo);
-            initialValues.put(Cuerpo,cuerpo);
+
+
+    public long insertHistoria(int id, String titulo, String genero, String cuerpo) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_IDINFO, id);
+        initialValues.put(Titulo, titulo);
+        initialValues.put(Genero, genero);
+        initialValues.put(Cuerpo, cuerpo);
         try {
-            return mDb.insertOrThrow(DATABASE_TABLE_INFORMACION, null, initialValues);
-        }catch (SQLiteConstraintException e){
+            return mDb.insertOrThrow(DATABASE_TABLE_HISTORIAS, null, initialValues);
+        } catch (SQLiteConstraintException e) {
             return 0;
         }
     }
 
-    public long insertMultimedia(String tipo, String uri, int duracion, String textomulti){
-        if(tipo==null || uri ==null){
-            Log.w("InsertMultimediaError","tipo:"+tipo+" uri:"+uri);
-            return -1;
-        }else {
-            ContentValues initialValues = new ContentValues();
-            initialValues.put(Tipo, tipo);
-            initialValues.put(URI, uri);
-            initialValues.put(TextoMulti, textomulti);
-            initialValues.put(Duracion, duracion);
-            try {
-                return mDb.insertOrThrow(DATABASE_TABLE_MULTIMEDIA, null, initialValues);
-            }catch (SQLiteConstraintException e){
-                e.printStackTrace();
-                return 0;
-            }
-        }
-    }
 
-    public long insertPregunta(int id, String texto, String correcta, String error1, String error2, String error3,
-                               String error4, String error5){
+    public long insertTarjeta(int id, String Nombre, String Foto, String cuerpo, int derecha, String opcionD,  int izquierda, String opcionI, int historia) {
         ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_IDPREG,id);
-        initialValues.put(Texto, texto);
-        initialValues.put(Correcta,correcta);
-        initialValues.put(Error1, error1);
-        initialValues.put(Error2, error2);
-        initialValues.put(Error3, error3);
-        initialValues.put(Error4, error4);
-        initialValues.put(Error5, error5);
-
-        return mDb.insert(DATABASE_TABLE_PREGUNTA, null, initialValues);
-    }
-    public Cursor getAllImages(){
-        return mDb.query(DATABASE_TABLE_MULTIMEDIA,new String[]{"*"},Tipo+"=?",new String[]{"IMAGE"},null,null,null);
-    }
-    public long insertPregunta(int id, String texto, String correcta, String e1, String e2, String e3){
-        ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_IDPREG,id);
-        initialValues.put(Texto,texto);
-        initialValues.put(Correcta,correcta);
-        initialValues.put(Error1,e1);
-        initialValues.put(Error2,e2);
-        initialValues.put(Error3,e3);
-        try{
-        return mDb.insertOrThrow(DATABASE_TABLE_PREGUNTA,null,initialValues);
-        }catch (SQLiteConstraintException e){
+        initialValues.put(KEY_IDINFO, id);
+        initialValues.put(Nombre, Nombre);
+        initialValues.put(Foto, Foto);
+        initialValues.put(Cuerpo, cuerpo);
+        initialValues.put(Derecha, derecha);
+        initialValues.put(OpcionDerecha, opcionD);
+        initialValues.put(Izquierda,izquierda);
+        initialValues.put(OpcionIzquierda, opcionI);
+        initialValues.put(HISTORIA,historia);
+        try {
+            return mDb.insertOrThrow(DATABASE_TABLE_HISTORIAS, null, initialValues);
+        } catch (SQLiteConstraintException e) {
             return 0;
         }
     }
-    public Cursor getQuestion(int id){
-        return mDb.query(DATABASE_TABLE_PREGUNTA,new String[]{"*"},KEY_IDPREG+"=?",new String[]{""+id},null,null,null);
-    }
-    public Cursor getCountQuestions(){
-        return mDb.query(DATABASE_TABLE_PREGUNTA,new String[]{"COUNT(*)"},null,null,null,null,null);
-    }
-    public Cursor getAllQuestions(){
-        return mDb.query(DATABASE_TABLE_PREGUNTA,new String[]{"*"},null,null,null,null,null);
+    public Cursor returnAllHistorias() {
+        return mDb.query(DATABASE_TABLE_HISTORIAS, null,null,null,null,null,null);
     }
 
-    public Cursor getAllInformation(){
-        return mDb.query(DATABASE_TABLE_INFORMACION,new String[]{"*"},null,null,null,null,null);
-    }
-
-    public Cursor getTitleInformation(int idInfo){
-        return mDb.query(DATABASE_TABLE_INFORMACION, new String[]{Titulo},KEY_IDINFO+"=?",new String[]{""+idInfo},null,null,null,null);
-    }
-    public Cursor getBodyInformation(int idInfo){
-        return mDb.query(DATABASE_TABLE_INFORMACION, new String[]{Cuerpo},KEY_IDINFO+"=?",new String[]{""+idInfo},null,null,null,null);
-    }
-
-/*
-    public long insertPartida(int id, int puntuacion, int//OISBIWUVECVW fecha,String juego, int numero){
-        ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_IDPART,id);
-        initialValues.put(, tipo);
-        initialValues.put(URI, uri);
-        initialValues.put(Duracion, duracion);
-
-        return mDb.insert(DATABASE_TABLE_PARTIDA, null, initialValues);
-    }
-    */
-    /*
-    public long insertNewPlaylist(String nombrePlaylist){
-        if(nombrePlaylist.length()>0){
-            ContentValues initialValues = new ContentValues();
-            initialValues.put(KEY_NOMBRE_PLAYLIST,nombrePlaylist);
-            initialValues.put(KEY_DURACION_PLAYLIST, 0);
-            initialValues.put(KEY_NUM_CANCIONES,0);
-            return mDb.insert(DATABASE_TABLE_PLAYLIST, null, initialValues);
-        }else{
-            Log.d(TAG,"Insercción erronea ("+nombrePlaylist+","+0+","+ 0);
-            return -1;
-        }
-    }
-    public long insertToPlaylist(String rutaCancion, String playlist){
-        if(rutaCancion.length()>0 && playlist.length()>0){
-            ContentValues initialValues = new ContentValues();
-            initialValues.put(KEY_CANCION_PERTENECE,rutaCancion);
-            initialValues.put(KEY_NOM_PLAYLIST_PERTENCE, playlist);
-            return mDb.insert(DATABASE_TABLE_PERTENECE, null, initialValues);
-        }else{
-            Log.d(TAG,"Insercción erronea ("+rutaCancion+","+playlist);
-            return -1;
-        }
-    }
-
-    public Cursor getAllCancion() {
-            return mDb.query(DATABASE_TABLE_CANCION, new String[] {"*"}, null, null,null , null,KEY_TITULO);
-    }
-    public Cursor getAllFromPlaylist(String playlist){
-        switch (playlist){
-            case DEFAULT_PLAYLIST_TODAS:
-                return getAllCancion();
-            case DEFAULT_PLAYLIST_FAVORITOS:
-                return  mDb.query(DATABASE_TABLE_CANCION, new String[] {"*"}, KEY_FAVORITO+" = 1", null,null , null,KEY_TITULO);
-            default:
-                String[] playlist_array = {playlist};
-
-                //Creamos el constructor de consultas
-                SQLiteQueryBuilder QB = new SQLiteQueryBuilder();
-
-                //Especificamos el JOIN
-                QB.setTables(DATABASE_TABLE_CANCION +
-                      " LEFT OUTER JOIN " + DATABASE_TABLE_PERTENECE + " ON " +
-                      KEY_RUTA + " = " + KEY_CANCION_PERTENECE);
-                //Especificamos el select, en este caso, todos los atributos de la tabla Cancion
-                String[] select = {KEY_TITULO,KEY_RUTA,KEY_REPRODUCCIONES,KEY_FAVORITO,KEY_DURACION};
-                //Consultamos y devolvemos resultado
-                return QB.query(mDb, select, KEY_NOM_PLAYLIST_PERTENCE+"=?", playlist_array, null, null, KEY_TITULO);
-        }
+    public Cursor returnAllFromHistoria(int id) {
+        return mDb.query(DATABASE_TABLE_TARJETAS, null,HISTORIA+ "=" + id,null,null,null,null);
     }
 
 
-    //devuelve todas las playlist
-    public Cursor getAllPlaylist(){
-        return mDb.query(DATABASE_TABLE_PLAYLIST,new String[]{KEY_NOMBRE_PLAYLIST,KEY_NUM_CANCIONES,KEY_DURACION_PLAYLIST},null,null,null,null,KEY_NOMBRE_PLAYLIST);
+    public Cursor returnTarjeta(int historia, int id){
+        return mDb.query(DATABASE_TABLE_TARJETAS, null,HISTORIA+ "=" + historia + " AND " + KEY_IDINFO + "=" + id,null,null,null,null);
     }
 
-    //comprueba si una playlist esta en la bd
-    public boolean comprobarPlaylist(String nombre){
-        Cursor c= getAllPlaylist();
-        boolean pertenece  = false;
-        if(c.moveToFirst()){
-            do {
-                String ruta = c.getString(c.getColumnIndex(DbAdapter.KEY_NOMBRE_PLAYLIST));
-                if(ruta.equals(nombre)){
-                    pertenece = true;
-                    return pertenece;
-                }
-            } while(c.moveToNext());
-        }
-        return  pertenece;
+    public void updateLastCard(int Historia, int id){
+        mDb.execSQL("UPDATE "+DATABASE_TABLE_HISTORIAS+" SET "+UltimaTarjeta+"="+id+" WHERE "+KEY_IDINFO+"="+Historia);
     }
-//TODO NO SE DEBERIA USAR ESTO
-    /*
-    //devuelve numero de canciones en una playlist
-    /*public int  getNumero(String playlist){
-        return mDb.
-    }
-
-    //devuelve la duracion de una playlist
-
-    //devuelve el nombre de una cancion dada su ruta
-    public  Cursor nombre_cancion(String ruta){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_TITULO},KEY_RUTA+"="+ruta,null,null,null,KEY_TITULO);
-    }
-    //devuelve duracion
-   public  Cursor DURACION_CANCION(String ruta){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_DURACION},KEY_RUTA+"="+ruta,null,null,null,KEY_DURACION);
-    }
-
-    //devuelve reproduccion
-    public  Cursor num_reproduccion(String ruta){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_REPRODUCCIONES},KEY_RUTA+"="+ruta,null,null,null,KEY_REPRODUCCIONES);
-    }
-
-    //devuelve favorito
-    public  Cursor es_favorito(String ruta){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_FAVORITO},KEY_RUTA+"="+ruta,null,null,null,KEY_FAVORITO);
-    }
-
-    public  Cursor ruta (String nombre){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_RUTA},KEY_TITULO+"="+nombre,null,null,null,KEY_RUTA);
-    }
-    //devuelve duracion
-    public  Cursor DURACION_CANCION_nom(String  nombre){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_DURACION},KEY_TITULO+"="+nombre,null,null,null,KEY_DURACION);
-    }
-
-    //devuelve reproduccion
-    public  Cursor num_reproduccion_nom(String nombre){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_REPRODUCCIONES},KEY_TITULO+"="+nombre,null,null,null,KEY_REPRODUCCIONES);
-    }
-
-    //devuelve favorito
-    public  Cursor es_favorito_nom(String nombre){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_FAVORITO},KEY_TITULO+"="+nombre,null,null,null,KEY_FAVORITO);
-    }
-
-    //devuelve lista con nombre de favoritos
-    public  Cursor lista_nombre_favoritos(){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_TITULO},KEY_FAVORITO+"="+1,null,null,null,null,KEY_TITULO);
-    }
-
-    //devuelve lista con rutas de favoritos
-    public  Cursor lista_ryta_favoritos(){
-        return mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_RUTA},KEY_FAVORITO+"="+1,null,null,null,KEY_RUTA);
-    }
-
-
-
-
-
-    //Metodo para actualizar un playlist
-   public void actualizar_playlist(String playlist, long dur, int num){
-       //TODO no hace falta, esto lo hará un trigger
-       Log.i("SQLite", "UPDATE: KEY_NOMBRE_PLAYLIST=" + playlist+"-"+dur+","+num);
-        ContentValues values = new ContentValues();
-        values.put(KEY_NOMBRE_PLAYLIST,playlist);
-        values.put(KEY_DURACION_PLAYLIST,dur);
-        values.put(KEY_NUM_CANCIONES,num);
-        mDb.update(playlist,values,KEY_NOMBRE_PLAYLIST+"="+ playlist,null);
-    }
-
-
-    public boolean cambiar_nombre_playlist(String nombre_viejo, String nombre_nuevo){
-        if(insertNewPlaylist(nombre_nuevo)<0){
-            return false;
-        }
-        else{
-            Log.d(nombre_nuevo,"insertado");
-        }
-        Cursor c= getAllFromPlaylist(nombre_viejo);
-        if(c.moveToFirst()){
-
-            do {
-                String ruta = c.getString(c.getColumnIndex(DbAdapter.KEY_RUTA));
-                Log.d(nombre_nuevo, ruta);
-
-                if(insertToPlaylist(ruta, nombre_nuevo)<0){
-
-                    return false;
-                }
-                else{
-                    Log.d("insertado",ruta);
-                }
-            } while(c.moveToNext());
-        }
-        return deletePlaylist(nombre_viejo);
-
-
-    }
-
-    public  Cursor getNumSongFromPlaylist(String playlist){
-        String[] playlist_array = {playlist};
-        return mDb.query(DATABASE_TABLE_PERTENECE, new String[]{"COUNT("+KEY_CANCION_PERTENECE+")"},KEY_NOM_PLAYLIST_PERTENCE +"= ?",playlist_array,null,null,KEY_CANCION_PERTENECE);
-    }
-
-    public  Cursor getDuracionLista(String playlist){
-        String[] playlist_array = {playlist};
-        return mDb.query(DATABASE_TABLE_PLAYLIST,new String[]{KEY_DURACION_PLAYLIST},KEY_NOMBRE_PLAYLIST+" =?",playlist_array,null,null,KEY_NOMBRE_PLAYLIST);
-    }
-
-    public boolean deleteCancion(String cancion){
-
-       boolean borrar;
-        boolean borrado;
-        borrar = mDb.delete(DATABASE_TABLE_PERTENECE,KEY_CANCION_PERTENECE+"=?", new String[] {cancion})>0;
-        if(borrar){
-            Log.d("borrado","cancion de pertenece");
-        }
-
-        else{
-            Log.d("no borrado","cancion de pertenece");
-        }
-        borrado = mDb.delete(DATABASE_TABLE_CANCION,KEY_RUTA+"=?", new String[] {cancion})>0;
-        if(borrado){
-            Log.d("borrado","cancion de tabla");
-        }
-
-        else{
-            Log.d("no borrado","cancion de tabla");
-        }
-        return borrar&&borrado;
-    }
-
-    public boolean deletePlaylist(String playlist){
-        boolean borrar;
-        boolean borrado;
-        borrar = mDb.delete(DATABASE_TABLE_PERTENECE,KEY_NOM_PLAYLIST_PERTENCE+"=?", new String[] {playlist})>0;
-        if(borrar){
-            Log.d("borrado","playlist de pertenece");
-        }
-
-        else{
-            Log.d("no borrado","playlist de pertenece");
-        }
-        borrado = mDb.delete(DATABASE_TABLE_PLAYLIST,KEY_NOMBRE_PLAYLIST+"=?", new String[] {playlist})>0;
-        if(borrado){
-            Log.d("borrado","playlist de tabla");
-        }
-
-        else{
-            Log.d("no borrado","playlist de tabla");
-        }
-        return borrar&&borrado;
-
-
-    }
-
-
-    public boolean deleteFromPlaylist(String cancion, String playlist){
-           return mDb.delete(DATABASE_TABLE_PERTENECE,KEY_CANCION_PERTENECE+"=?"+" AND "+KEY_NOM_PLAYLIST_PERTENCE+"=?",new String[]{cancion,playlist})>0;
-
-    }
-
-
-   /* public int incrementarReproduccionesCancion(String ruta){
-
-
-
-
-        Cursor c = mDb.query(DATABASE_TABLE_CANCION, new String[] {KEY_REPRODUCCIONES}, KEY_RUTA+"= ?"+ruta, null,null , null,null);
-
-
-
-
-
-
-    }
-    public void incrementarReproduccionCancion(String ruta){
-        Cursor c = mDb.query(DATABASE_TABLE_CANCION, new String[]{KEY_REPRODUCCIONES} ,KEY_RUTA+"= ?",new String[]{ruta},null,null,null,null);
-
-        c.moveToFirst();
-        int reproducciones = c.getInt(c.getColumnIndex(DbAdapter.KEY_REPRODUCCIONES));
-
-        ContentValues args = new ContentValues();
-        reproducciones++;
-        args.put(KEY_REPRODUCCIONES, reproducciones);
-        mDb.update(DATABASE_TABLE_CANCION, args, KEY_RUTA + " =?", new String[]{ruta});
-
-
-
-    }*/
-
-
 }
+
