f3e17217676bee49fa72c01ec4a9580f742b48cd
gcebollero <guillermocebollero@gmail.com>
2017-04-12 23:59:51 +0200

    Initialize db connection and fix some errors

--- app/src/main/java/com/mayc/unizar/app/factories/ItemFactory.java
+++ app/src/main/java/com/mayc/unizar/app/factories/ItemFactory.java
@@ -2,6 +2,7 @@ package com.mayc.unizar.app.factories;
 
 
 import android.database.Cursor;
+import android.util.Log;
 
 import com.mayc.unizar.app.adapters.DbAdapter;
 import com.mayc.unizar.app.types.Item;
@@ -13,21 +14,28 @@ import com.mayc.unizar.app.types.Item;
 public class ItemFactory {
 
     public DbAdapter db;
+    private static final String TAG = "ItemFactory";
+    public ItemFactory(DbAdapter db){
+        this.db=db;
+    }
 
 
-    public Item getCard(int id, int Historia){
-       Cursor cur = db.returnTarjeta(Historia, id);
-        if (cur.moveToFirst()){
+    public Item getCard(int id, int historia){
+       Cursor cur = db.returnTarjeta(historia, id);
+        if (cur!=null && cur.moveToFirst()){
+            Log.d(TAG, "getCard: Generating card");
             return new Item(cur.getInt(0), cur.getString(1), cur.getString(2), cur.getString(3), cur.getString(4), cur.getInt(5), cur.getString(6), cur.getInt(7));
         }
+        Log.d(TAG, "getCard: Returning null");
         return new Item(-1);
    }
 
 
 
-   public Item[] allCards(int Historia){
-       Cursor c = db.returnAllFromHistoria(Historia);
+   public Item[] allCards(int historia){
+       Cursor c = db.returnAllFromHistoria(historia);
        Item [] list = new Item[c.getCount()];
+       c.moveToFirst();
        int i=0;
        do {
            list[i] = new Item(c.getInt(0), c.getString(1), c.getString(2), c.getString(3), c.getString(4), c.getInt(5), c.getString(6), c.getInt(7));
